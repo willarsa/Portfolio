4f770e23bc2829297ff7b35834acb8521f8b8d6c
Sam Willard <144750636+willarsa@users.noreply.github.com>
2025-07-26 18:04:36 -0700

    added resume, some animation, and linked images

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 function optionClick(elem){
     lines = document.getElementsByClassName('uline');
+    content = document.getElementsByClassName('content');
+
     for(let i = 0; i < lines.length; i++){
         if(lines[i] == elem.nextElementSibling){
             lines[i].classList.add('active');
+            content[i].classList.remove('invisible');
         }
         else{
             lines[i].classList.remove('active');
+            content[i].classList.add('invisible');
         }
     }
 }
