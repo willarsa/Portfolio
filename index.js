42461c5f50624e35373df2945dd2f9aa56619e0b
Sam Willard <144750636+willarsa@users.noreply.github.com>
2025-11-13 12:15:13 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ cmd.id = "cmd";
 cmd.textContent = "_";
 cmd.classList.add("cmd");
 
-var about = document.getElementById("about");
+var about;
 
 var lines = ["Hello, I'm Sam Willard, a 3rd year computer science and mathematics honors student at Oregon State University",
              "My focus for CS is in Computer Systems, where I'm learning a lot about lower-level hardware and it's implementations"];
@@ -25,7 +25,16 @@ function toggle(project){
 }
 
 document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("about").appendChild(cmd);
+    about = document.getElementById("about");
+    about.appendChild(cmd);
+
+    about.ontouchstart = function(){
+        if(lines.length > 0){
+            cmd.parentElement.removeChild(cmd);
+            var text = lines.shift();
+            addConsoleLine(text, 1);
+        }
+    };
 });
 
 function pause (milliseconds) {
@@ -42,14 +51,6 @@ document.addEventListener("keydown", async function(e) {
         } 
     }
 });
-
-about.ontouchstart = function(){
-    if(lines.length > 0){
-        cmd.parentElement.removeChild(cmd);
-        var text = lines.shift();
-        addConsoleLine(text, 1);
-    }
-};
 
 async function addConsoleLine(text, check){
     if(check != 1) cmd.parentElement.removeChild(cmd);
