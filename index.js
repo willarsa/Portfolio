aab99ccaa1d99a6f19fce38ea2dca2d9b4bb9629
Sam Willard <144750636+willarsa@users.noreply.github.com>
2025-11-13 12:07:39 -0800

    Mobile Support For Command Line

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ cmd.id = "cmd";
 cmd.textContent = "_";
 cmd.classList.add("cmd");
 
+var about = document.getElementById("about");
+
 var lines = ["Hello, I'm Sam Willard, a 3rd year computer science and mathematics honors student at Oregon State University",
              "My focus for CS is in Computer Systems, where I'm learning a lot about lower-level hardware and it's implementations"];
 
@@ -40,6 +42,17 @@ document.addEventListener("keydown", async function(e) {
         } 
     }
 });
+
+about.ontouchstart = function(){
+    if(lines.length > 0){
+       cmd.parentElement.removeChild(cmd);
+
+        if(e.key == "Enter"){
+            var text = lines.shift();
+            addConsoleLine(text, 1);
+        } 
+    }
+};
 
 async function addConsoleLine(text, check){
     if(check != 1) cmd.parentElement.removeChild(cmd);
